9351bae01128c2539448ce5e7681a60220acbd20
mohamadnosratian <68176509+mohamadnosratian@users.noreply.github.com>
2020-11-07 15:46:45 -0600

    convert gold.py functionality to a function for usage in handler.py

--- core/gold.py
+++ core/gold.py
@@ -3,21 +3,22 @@ import json
 from bs4 import BeautifulSoup as BS, element
 import re as Regex
 
-# take the config file with all regexes and urls and ...
-with open('./core/config.json') as json_data:
-    config = json.load(json_data)
 
+def Gold_Corrent_Prise(config):
+    """
+    with web scraping get the corrent future gold price from tgju.org
+    """
 
-SiteData = req.get(config["Gold"]["url"], headers=config["Header"])
-print(SiteData.status_code)
+    SiteData = req.get(config["Gold"]["url"], headers=config["Header"])
+    print(SiteData.status_code)
 
-SoupData = BS(SiteData.text, "html.parser").find_all('tbody',
-                                                     attrs={'class': 'table-padding-lg'})
+    SoupData = BS(SiteData.text, "html.parser").find_all('tbody',
+                                                         attrs={'class': 'table-padding-lg'})
 
-# test the soup output
-""" with open('./core/output', 'w') as output:
-    output.write(SoupData[0].text)
- """
+    # test the soup output
+    """ with open('./core/output', 'w') as output:
+        output.write(SoupData[0].text)
+    """
 
-CorrentPrice = Regex.findall(config["Gold"]["regex"], SoupData[0].text)[0]
-print(CorrentPrice.replace(',', ''))
+    CorrentPrice = Regex.findall(config["Gold"]["regex"], SoupData[0].text)[0]
+    return CorrentPrice
