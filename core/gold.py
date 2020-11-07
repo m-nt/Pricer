4b23e825c8bff32611090ca1ed42cb5caa751f52
mohamadnosratian <68176509+mohamadnosratian@users.noreply.github.com>
2020-11-07 15:37:39 -0600

    get the corrent price from site

--- core/gold.py
+++ core/gold.py
@@ -1,12 +1,23 @@
 import requests as req
 import json
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup as BS, element
 import re as Regex
 
 # take the config file with all regexes and urls and ...
-with open('config.json') as json_data:
+with open('./core/config.json') as json_data:
     config = json.load(json_data)
 
 
-sitedata = req.get(config["Gold"]["url"], headers=config["Header"])
-print(sitedata.status_code)
+SiteData = req.get(config["Gold"]["url"], headers=config["Header"])
+print(SiteData.status_code)
+
+SoupData = BS(SiteData.text, "html.parser").find_all('tbody',
+                                                     attrs={'class': 'table-padding-lg'})
+
+# test the soup output
+""" with open('./core/output', 'w') as output:
+    output.write(SoupData[0].text)
+ """
+
+CorrentPrice = Regex.findall(config["Gold"]["regex"], SoupData[0].text)[0]
+print(CorrentPrice.replace(',', ''))
